897fa99140493275e2ba92a8e2a492b2ad2ecb64
Connor O'Gara <ogara.connor@gmail.com>
2016-07-08 13:24:25 -0500

    finish assignment

---
+++ app.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+
+var numArray = [];
+//Read the number file
+//add number string
+
+function numsToArray(callback){
+  fs.readFile('numbers.txt', 'utf-8', function(err, fileContents) {
+    numArray = fileContents.split(', ');
+    for (var i = 0; i < numArray.length; i++) {
+      numArray[i] = parseInt(numArray[i]);
+    }
+    callback();
+  })
+}
+
+function done(array) {
+  console.log(getValues(numArray));
+}
+
+function getValues(array) {
+  var values = {};
+  values.high = array[0];
+  values.low = array[0];
+  values.average = 0;
+  var tempSum = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > values.high) {
+      values.high = array[i];
+    }
+    if (array[i] < values.low) {
+      values.low = array[i];
+    }
+    tempSum += array[i];
+  }
+  values.average = Math.round(tempSum/(numArray.length));
+  return values;
+}
+
+numsToArray(done);
